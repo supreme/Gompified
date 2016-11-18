e7046f3f340301bd2ae551555828f7b5c583cd8f
supreme <sandrews.r@gmail.com>
2016-11-18 17:12:44 -0500

    Clean up code

--- steez.py
+++ steez.py
@@ -12,22 +12,22 @@ import json
 from modules.Event import Event
 from modules.EventList import EventList
 
-target = 'https://orgsync.com/412/community/calendar?view=upcoming'
+TARGET = 'https://orgsync.com/412/community/calendar?view=upcoming'
 
 print('Setting up user agent...')
 #################################
-user_agent = (
+USER_AGENT = (
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " +
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36"
 )
 dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = user_agent
+dcap["phantomjs.page.settings.userAgent"] = USER_AGENT
 
 print('Setting up Phantom JS webdriver...')
 ###########################################
 browser = webdriver.PhantomJS(desired_capabilities=dcap)
 
-print('Fetching - ', target)
+print('Fetching - ', TARGET)
 ############################
 browser.get('https://orgsync.com/412/community/calendar?view=upcoming')
 
@@ -39,28 +39,26 @@ events_module = browser.find_element_by_class_name('osw-events-index-main')
 print('Found events module')
 
 # Constants specific to Orgsync DOM
-find_date = {'name': 'div', 'attrs': {'class': 'osw-events-list-date'}}
-find_events = {'name': 'a', 'attrs': {'class': 'osw-events-list-item-content'}}
-title_class = 'osw-events-list-date-header'
-name_attr = {'name': 'div', 'attrs': {'class': 'osw-events-list-date'}}
-date_attr = {'name': 'div', 'attrs': {'class': 'osw-events-list-date'}}
-
-def find(tag, attr_type, attr_name, attr_val):
-    return tag.find_all(attr_type, attrs=dict([(attr_name, attr_val)]))
+DATE_CLASS = 'osw-events-list-date'
+EVENT_CLASS = 'osw-events-list-item-content'
+TITLE_CLASS = 'osw-events-list-date-header'
+NAME_CLASS = 'osw-events-list-item-title'
+TIME_CLASS = 'osw-events-list-item-time'
+ORG_CLASS = 'osw-events-list-item-portal-name'
 
 # Parse remaining with BS4
 html = events_module.get_attribute('outerHTML')
 soup = BeautifulSoup(html, 'html.parser')
-dates = soup.find_all('div', attrs={'class': 'osw-events-list-date'})
+dates = soup.find_all('div', attrs={'class': DATE_CLASS})
 calendar = []
 for item in dates: # <class 'bs4.element.Tag'>
-    title = item.find('div', attrs={'class', title_class})
-    events = item.findAll('a', attrs={'class': 'osw-events-list-item-content'})
+    title = item.find('div', attrs={'class', TITLE_CLASS})
+    events = item.findAll('a', attrs={'class': EVENT_CLASS})
     event_list = EventList(title.text, [])
     for entry in events: # Events in November 18th, 2016 <- example
-        name = entry.find('div', attrs={'class': 'osw-events-list-item-title'}).text
-        time = entry.find('span', attrs={'class': 'osw-events-list-item-time'}).text
-        organization = entry.find('span', attrs={'class': 'osw-events-list-item-portal-name'}).text
+        name = entry.find('div', attrs={'class': NAME_CLASS}).text
+        time = entry.find('span', attrs={'class': TIME_CLASS}).text
+        organization = entry.find('span', attrs={'class': ORG_CLASS}).text
         img = ''
         try: # Some events don't have images
             img = entry.find('img')['src']
@@ -68,7 +66,7 @@ for item in dates: # <class 'bs4.element.Tag'>
             event_list.errors.append('Missing image for event: {}'.format(name))
         event = Event(name, time, organization, img)
         event_list.events.append(event)
-    print(json.dumps(event_list.to_json(), indent=4, sort_keys=True))
+    print(json.dumps({'results': event_list.to_json()}, indent=4, sort_keys=True))
 
 browser.close()
 
